8fd9e42b759d096312ce9afd45cac3f23aa95a7d
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2022-05-12 16:12:29 +0900

    [CLEANUP] simplify heat3d openmp example

--- heat3d/openmp/Timestep.hpp
+++ heat3d/openmp/Timestep.hpp
@@ -3,13 +3,10 @@
 
 #include "Config.hpp"
 #include "../Timer.hpp"
-#include "boundary.hpp"
 
-void step(Config &conf, Boundary &boundary, RealView3D &u, RealView3D &un, std::vector<Timer*> &timers);
+void step(Config &conf, RealView3D &u, RealView3D &un, std::vector<Timer*> &timers);
 
-void step(Config &conf, Boundary &boundary, RealView3D &u, RealView3D &un, std::vector<Timer*> &timers) {
-  boundary.exchangeHalos(conf, u, timers);
-
+void step(Config &conf, RealView3D &u, RealView3D &un, std::vector<Timer*> &timers) {
   const int nx = conf.nx, ny = conf.ny, nz = conf.nz;
   const float64 coef = conf.Kappa * conf.dt / (conf.dx*conf.dx);
   
@@ -23,10 +20,16 @@ void step(Config &conf, Boundary &boundary, RealView3D &u, RealView3D &un, std::
     for(int iy=0; iy<ny; iy++) {
       LOOP_SIMD
       for(int ix=0; ix<nx; ix++) {
+        const int ixp1 = (ix + nx + 1) % nx;
+        const int ixm1 = (ix + nx - 1) % nx;
+        const int iyp1 = (iy + ny + 1) % ny;
+        const int iym1 = (iy + ny - 1) % ny;
+        const int izp1 = (iz + nz + 1) % nz;
+        const int izm1 = (iz + nz - 1) % nz;
         un(ix, iy, iz) = u(ix, iy, iz)
-          + coef * ( u(ix+1, iy, iz) + u(ix-1, iy, iz)
-                   + u(ix, iy+1, iz) + u(ix, iy-1, iz)
-                   + u(ix, iy, iz+1) + u(ix, iy, iz-1)
+          + coef * ( u(ixp1, iy, iz) + u(ixm1, iy, iz)
+                   + u(ix, iyp1, iz) + u(ix, iym1, iz)
+                   + u(ix, iy, izp1) + u(ix, iy, izm1)
                    - 6. * u(ix, iy, iz) );
       }
     }
