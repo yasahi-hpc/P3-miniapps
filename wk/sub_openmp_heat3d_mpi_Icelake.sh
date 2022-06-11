9895d6836fd04a7af3ecfbfcbdcb85df7864065d
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2022-06-11 17:57:51 +0900

    Add job scripts for openmp backend

--- wk/sub_openmp_heat3d_mpi_Icelake.sh
+++ wk/sub_openmp_heat3d_mpi_Icelake.sh
@@ -4,7 +4,7 @@
 #PJM -L "elapse=10:00"
 #PJM -s
 #PJM -g jh220031a
-#PJM --mpi proc=2
+#PJM --mpi proc=1
 
 module purge
 module load intel/2021.2.0 impi/2021.2.0 fftw/3.3.9
@@ -15,5 +15,7 @@ export UCX_IB_GPU_DIRECT_RDMA=no
 export OMP_NUM_THREADS=36
 export OMP_PROC_BIND=true
 
-mpiexec.hydra -n 2 \
-    ../build/heat3d_mpi/openmp/heat3d_mpi --px 2 --py 1 --pz 1 --nx 256 --ny 512 --nz 512 --nbiter 1000
+mpiexec.hydra -n 1 \
+    ../build/heat3d_mpi/openmp/heat3d_mpi --px 1 --py 1 --pz 1 --nx 512 --ny 512 --nz 512 --nbiter 1000 --freq_diag 0
+#mpiexec.hydra -n 2 \
+#    ../build/heat3d_mpi/openmp/heat3d_mpi --px 1 --py 1 --pz 2 --nx 512 --ny 512 --nz 256 --nbiter 1000 --freq_diag 0
--- wk/sub_openmp_vlp4d_mpi_icelake.sh
+++ wk/sub_openmp_vlp4d_mpi_icelake.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#PJM -L "node=2"
+#PJM -L "node=1"
 #PJM -L "rscgrp=regular-a"
 #PJM -L "elapse=10:00"
 #PJM -s
 #PJM -g jh220031a
-#PJM --mpi proc=16
+#PJM --mpi proc=2
 
 module purge
 module load intel/2021.2.0 impi/2021.2.0 fftw/3.3.9
 
-export OMP_NUM_THREADS=9
+export OMP_NUM_THREADS=36
 export OMP_PROC_BIND=true
 
 export UCX_MEMTYPE_CACHE=n
 export UCX_IB_GPU_DIRECT_RDMA=no
 
-mpiexec.hydra -n 16 \
-    ../build/vlp4d_mpi/openmp/vlp4d_mpi --num_threads 9 --teams 1 --device 0 --num_gpus 8 --device_map 1 -f SLD10.dat
+mpiexec.hydra -n 2 \
+    ../build/vlp4d_mpi/openmp/vlp4d_mpi --num_threads 36 --teams 1 --device 0 --num_gpus 8 --device_map 1 -f SLD10.dat
