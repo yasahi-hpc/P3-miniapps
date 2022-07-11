6258353b663bebf689f2de469e6367fe3e7f0bbe
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2022-07-11 15:08:41 +0900

    [job scripts] Update job scripts for V100

---
+++ wk/sub_kokkos_heat3d_mpi_V100_2MPI.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -q sg8
+#PBS -l select=1:ncpus=24:mpiprocs=2:ompthreads=12:ngpus=2
+#PBS -l walltime=01:00:00
+#PBS -P CityLBM@PG22010
+
+cd $PBS_O_WORKDIR
+. /etc/profile.d/modules.sh
+
+module purge
+module load cuda/11.0 gnu/7.4.0 mpt/2.23-ga
+
+export MPI_SHEPHERD=1
+export MPI_USE_CUDA=1
+
+mpirun -np 2 ../build/miniapps/heat3d_mpi/kokkos/heat3d_mpi --px 1 --py 1 --pz 2 --nx 512 --ny 512 --nz 256 --nbiter 1000 --freq_diag 0 --num_threads 1 --teams 1 --device 0 --num_gpus 4 --device_map 1
