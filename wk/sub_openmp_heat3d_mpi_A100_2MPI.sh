7a6911984c623c3ed93b2462e9dc3c2c3cb44b60
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2022-07-13 18:27:57 +0900

    [job scripts] Update job scripts for A100

---
+++ wk/sub_openmp_heat3d_mpi_A100_2MPI.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PJM -L "node=1"
+#PJM -L "rscgrp=regular-a"
+#PJM -L "elapse=10:00"
+#PJM -s
+#PJM -g jh220031a
+#PJM --mpi proc=2
+
+module purge
+module load nvidia/22.5 nvmpi/22.5
+
+export UCX_MEMTYPE_CACHE=n
+export UCX_IB_GPU_DIRECT_RDMA=no
+
+mpiexec -machinefile $PJM_O_NODEINF -np $PJM_MPI_PROC -npernode 2 \
+    ./wrapper.sh ../build/miniapps/heat3d_mpi/openmp/heat3d_mpi --px 1 --py 1 --pz 2 --nx 512 --ny 512 --nz 256 --nbiter 1000 --freq_diag 0
--- wk/sub_thrust_heat3d_mpi_A100_2MPI.sh
+++ wk/sub_thrust_heat3d_mpi_A100_2MPI.sh
@@ -4,7 +4,7 @@
 #PJM -L "elapse=10:00"
 #PJM -s
 #PJM -g jh220031a
-#PJM --mpi proc=1
+#PJM --mpi proc=2
 
 module purge
 module load gcc/8.3.1
@@ -14,5 +14,5 @@ module load ompi-cuda/4.1.1-11.2
 export UCX_MEMTYPE_CACHE=n
 export UCX_IB_GPU_DIRECT_RDMA=no
 
-mpiexec -machinefile $PJM_O_NODEINF -np $PJM_MPI_PROC -npernode 1 \
-    ../build/miniapps/heat3d_mpi/thrust/heat3d_mpi --px 1 --py 1 --pz 1 --nx 512 --ny 512 --nz 512 --nbiter 1000 --freq_diag 0
+mpiexec -machinefile $PJM_O_NODEINF -np $PJM_MPI_PROC -npernode 2 \
+    ../build/miniapps/heat3d_mpi/thrust/heat3d_mpi --px 1 --py 1 --pz 2 --nx 512 --ny 512 --nz 256 --nbiter 1000 --freq_diag 0
